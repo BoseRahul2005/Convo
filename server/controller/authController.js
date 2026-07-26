bdf7f90901d282d11692676da2ec54b178c927ed
Rahul Bose <rahul.basu2005@gmail.com>
2026-07-26 18:57:10 +0530

    register,login and logout apis created. welcome mail generation created using nodemail

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,9 +1,10 @@
-import User from "../models/userModel.js";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+const User=require("../models/userModel");
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
+const transporter=require("../config/nodemailer.js");
 
 //for registering a new user
-export const register=async (req, res) => {
+exports.register=async (req, res) => {
     const { username,name, email, password } = req.body;
     if(!username || !name || !email || !password){
         return res.json({success:false, message:"All fields are required"});
@@ -14,10 +15,12 @@ export const register=async (req, res) => {
         //checking if user already exists in the db by finding the user by email
         const existingUser = await User.findOne({ email });
         if(existingUser){
+            //if user already exists, we will return an error message
             return res.json({success:false, message:"User already exists"});
         }
-        const hashedPassword=await bcrypt.hash(password, 10);
+        const hashedPassword=bcrypt.hashSync(password, 10); //hashing the password before saving it to the database
 
+        //if user doesn't exist, we will create a new user object and save it to the database
         const newUser=new User({
             username,
             name,
@@ -25,9 +28,50 @@ export const register=async (req, res) => {
             password:hashedPassword,
         });
         await newUser.save();
-        res.json({success:true, message:"User registered successfully"});
-
+        
         const token=jwt.sign({id:newUser._id}, process.env.JWT_SECRET, {expiresIn:"7d"});
+        
+        res.cookie("token", token, {
+            httpOnly:true,
+            secure:process.env.NODE_ENV==="production",
+            sameSite:process.env.NODE_ENV==="production"?"none":"strict",
+            maxAge:7*24*60*60*1000, // 7 days
+        });
+
+        // Send welcome email
+        const mailOptions = {
+            from: process.env.SENDER_MAIL,
+            to: newUser.email,
+            subject: "Welcome to Convo!",
+            text: `Hello ${newUser.name},\n\nThank you for registering with us! We're excited to have you on board.\n\nWake up your Beast in the chat..`
+        };
+        await transporter.sendMail(mailOptions);
+
+        res.json({success:true, message:"Registered successfully"});
+
+    }catch(err){
+        res.json({success:false, message:err.message});
+    }
+}
+
+exports.login=async (req, res) => {
+    const { email, password } = req.body;
+    if(!email || !password){
+        return res.json({success:false, message:"All fields are required"});
+    }
+
+    try{
+        const user=await User.findOne({email});
+        if(!user){
+            return res.json({success:false, message:"User not found"});
+        }
+
+        const isPasswordCorrect=bcrypt.compareSync(password, user.password);
+        if(!isPasswordCorrect){
+            return res.json({success:false, message:"Incorrect password"});
+        }
+
+        const token=jwt.sign({id:user._id}, process.env.JWT_SECRET, {expiresIn:"7d"});
 
         res.cookie("token", token, {
             httpOnly:true,
@@ -35,6 +79,21 @@ export const register=async (req, res) => {
             sameSite:process.env.NODE_ENV==="production"?"none":"strict",
             maxAge:7*24*60*60*1000, // 7 days
         });
+
+        res.json({success:true, message:"logged in successfully"});
+    }catch(err){
+        res.json({success:false, message:err.message});
+    }
+}
+
+exports.logout=async (req, res) => {
+    try{
+        res.clearCookie("token", {
+            httpOnly:true,
+            secure:process.env.NODE_ENV==="production",
+            sameSite:process.env.NODE_ENV==="production"?"none":"strict",
+        });
+        res.json({success:true, message:"logged out successfully"});
     }catch(err){
         res.json({success:false, message:err.message});
     }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express =require("express");
 const path=require("path");
-const connectDB=require("./config/mongodb");
+const connectDB=require("./config/mongodb.js");
 const cookieParser=require("cookie-parser");
 const cors=require("cors");
+const router=require("./routes/authRoutes.js");
 require("dotenv/config");
 
 const app=express();
@@ -14,9 +15,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials:true}));
 
+//API routes
 app.get("/",(req,res)=>{
     res.send("root is working");
-})
+});
+
+app.use("/api/auth", router);
 
 app.listen(port,()=>{
     console.log(`server is listening on port ${port}...`);
